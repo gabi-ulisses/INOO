d3936b3072e843b77f3820e3f902a040d1be2686
gabi-ulisses <gabrielle.ulisses@aluno.ifp.edu.br>
2024-10-02 19:38:07 -0300

    Calculando média ponderada

--- aula1/exercicios/PointedAverage.java
+++ aula1/exercicios/PointedAverage.java
@@ -1,5 +1,22 @@
 package aula1.exercicios;
 
+import java.util.Scanner;
+
 public class PointedAverage {
     
+    public static void main(String[] arg){
+
+        int nota1, nota2;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe a primeira nota: ");
+        nota1 = entrada.nextInt();
+        System.out.print("Informe a segunda nota: ");
+        nota2 = entrada.nextInt();
+
+        double media = ((nota1 * 1)+(nota2 * 2))/(1 + 2);
+        
+        System.out.println("Média: "+media);
+    }
 }
