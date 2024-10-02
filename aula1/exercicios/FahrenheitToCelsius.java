92d5294a5984f024a622e8ffb0f2e359df2cd3a7
gabi-ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2024-10-02 19:48:55 -0300

    Fahrenheit em Celsius

--- aula1/exercicios/FahrenheitToCelsius.java
+++ aula1/exercicios/FahrenheitToCelsius.java
@@ -1,13 +1,20 @@
 package aula1.exercicios;
 
+import java.util.Scanner;
+
 public class FahrenheitToCelsius {
     
     public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
 
-        int Fahrenheit = 120;
+        int Fahrenheit;
+        double Celsius;
 
-        double Celsius = (Fahrenheit - 32)/1.8;
+        System.out.print("Informe a temperatura em Fahrenheit: ");
+        Fahrenheit = entrada.nextInt();
+        Celsius = (Fahrenheit - 32)/1.8;
     
-        System.out.println(Celsius);
+        System.out.println("Temperatura em graus Celsius: "+Celsius"°");
     }
 }
