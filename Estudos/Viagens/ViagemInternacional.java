d2c4221a5a6162416167bff468198f1488584cfc
Gabrielle Ulisses <82672178+gabi-ulisses@users.noreply.github.com>
2025-01-29 19:39:00 -0300

    Atualizar o Viagens

---
+++ Estudos/Viagens/ViagemInternacional.java
@@ -0,0 +1,3 @@
+. A subclasse 
+ViagemInternacional exige o número do passaporte, permite remarcação somente com 7 dias de 
+antecedência, mediante uma taxa mais elevada e sem a possibilidade de troca do destino.
